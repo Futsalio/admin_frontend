8347db8cd4d4c78ab698ed300e390e2ee482d3ef
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2017-11-11 23:49:46 +0700

    :pencil: EDITING | Framework for actions, reducers, store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { LocaleProvider } from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
 
+import Store from './stores/index.js';
 import Homepage from './components/Homepage/Homepage';
 import { NoMatch } from './components/404';
 
 const App = () => {
   return (
+    <Provider store={Store}>
       <LocaleProvider locale={enUS}>
         <BrowserRouter>
           <Switch>
@@ -16,6 +19,7 @@ const App = () => {
           </Switch>
         </BrowserRouter>
       </LocaleProvider>
+    </Provider>
   );
 }
 
