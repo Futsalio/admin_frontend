84f0bf9da35b3bc17ca5eba7f6f32ca8df105fc9
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2017-11-26 06:03:04 +0700

    :hammer: BUILDING | Login Role function

--- src/components/Partnerpage/Partnerpage.js
+++ src/components/Partnerpage/Partnerpage.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Button } from 'antd';
+import { Redirect } from 'react-router';
+
+import { checkLogin, logout, isOwner, isAdminFutsal } from '../../utils/auth';
 
 export default class Partnerpage extends React.Component {
   render() {
+    if( !checkLogin() || isOwner() || isAdminFutsal() ) {
+      return(
+        <Redirect to='/login' />
+      );
+    }
+
     return (
       <div>
         <p>Partnerpage</p>
-        <Button type="primary">Logout</Button>
+        <Button type="primary" onClick={logout}>Logout</Button>
       </div>
     );
   }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -8,6 +8,46 @@ export function checkLogin () {
 
 export function logout() {
   localStorage.removeItem("futsalio");
-  
+
   return window.location.href = '/login';
+}
+
+export function isSuperAdmin() {
+  let user = JSON.parse(localStorage.getItem("futsalio"));
+
+  if(user.role === 'SA') {
+    return true;
+  }
+
+  return false;
+}
+
+export function isAdmin() {
+  let user = JSON.parse(localStorage.getItem("futsalio"));
+
+  if(user.role === 'AF') {
+    return true;
+  }
+
+  return false;
+}
+
+export function isOwner() {
+  let user = JSON.parse(localStorage.getItem("futsalio"));
+
+  if(user.role === 'OT') {
+    return true;
+  }
+
+  return false;
+}
+
+export function isAdminFutsal() {
+  let user = JSON.parse(localStorage.getItem("futsalio"));
+
+  if(user.role === 'AT') {
+    return true;
+  }
+
+  return false;
 }
