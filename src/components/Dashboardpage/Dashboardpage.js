65e9405b0e7bbe2c3638074506835dde66d7bd07
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2017-11-26 20:24:04 +0700

    :hammer: BUILDING | Temporary Layout

--- src/components/Dashboardpage/Dashboardpage.js
+++ src/components/Dashboardpage/Dashboardpage.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Layout, Menu, Icon, Button, Breadcrumb } from 'antd';
 
 import './Dashboardpage.css';
 import { logout, getUserData } from '../../utils/auth';
 
+const { Header, Sider, Content, Footer } = Layout;
+
 export default class Dashboardpage extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
   componentDidMount() {
     document.body.className = 'base';
   }
@@ -13,12 +25,51 @@ export default class Dashboardpage extends React.Component {
     let data = getUserData();
 
     return (
-      <div>
-        <p>Dashboardpage</p>
-        <Button type = 'primary' onClick = {logout}>Logout</Button>
-        <p>{data.username}</p>
-        <p>{data.password}</p>
-      </div>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+            <Button type = 'primary' onClick = {logout}>Logout</Button>
+          </Header>
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: '#fff', minHeight: 1060 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Futsalio ©2017 All Right Reserved
+          </Footer>
+        </Layout>
+      </Layout>
     );
   }
 }
